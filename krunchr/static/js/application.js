47a3eaff33723bdd19182ed7e4945d06112b3ae3
linkyndy <linkyndy@gmail.com>
2014-04-12 11:39:27 +0300

    Fixed typos and fields to be displayed in visualization

--- krunchr/static/js/application.js
+++ krunchr/static/js/application.js
@@ -127,7 +127,7 @@ String.prototype.repeat = function(num) {
     if ($("#doughnut").length) {
       var ctx = $("#doughnut").get(0).getContext("2d");
       ctx.canvas.height = ctx.canvas.width = $("#doughnut").parent().width()
-      var doughnut = new Chart(ctx).Doughtnut($("#doughnut").data("data"), {})
+      var doughnut = new Chart(ctx).Doughnut($("#doughnut").data("data"), {})
 
       $(window).resize(function() {
         clearTimeout(resizeTimer)
